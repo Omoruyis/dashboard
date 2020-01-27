d120bd2b03f58287390dc3ad5df86a3fd32899e3
unknown <omoruyi.isaac97@gmail.com>
2020-01-27 17:04:17 +0100

    react-table changes

--- src/stations.js
+++ src/stations.js
@@ -1,48 +1,16 @@
 import React, { Component } from 'react';
 import Nav from './nav'
 import Sidebar from './sidebar'
-import ReactCollapsingTable from 'react-collapsing-table'
 
 class Stations extends Component {
     state = {
-        stations: 'current-link',
-        tripDetails: [
-            { name: 'Olalekan Ojo', bus: '#1233334', passengers: 200, date: '08/21/2019', mileage: 1200 },
-            { name: 'Marian Hassan', bus: '#1233334', passengers: 200, date: '08/21/2019', mileage: 1200 },
-            { name: 'Olalekan Ojo', bus: '#1233334', passengers: 200, date: '08/21/2019', mileage: 1200 },
-            { name: 'Olalekan Ojo', bus: '#1233334', passengers: 200, date: '08/21/2019', mileage: 1200 },
-            { name: 'Olalekan Ojo', bus: '#1233334', passengers: 200, date: '08/21/2019', mileage: 1200 },
-            { name: 'Olalekan Ojo', bus: '#1233334', passengers: 200, date: '08/21/2019', mileage: 1200 },
-            { name: 'Olalekan Ojo', bus: '#1233334', passengers: 200, date: '08/21/2019', mileage: 1200 },
-        ]
+        stations: 'current-link'
     }
 
 
 
     render() {
-        const { stations, tripDetails } = this.state
-        
-        const rows = tripDetails.map((cur, i) => {
-            return {
-                id: i + 1,
-                name: cur.name,
-                busAssigned: cur.bus,
-                passengers: cur.passengers,
-                tripDate: cur.date,
-                mileage: cur.mileage,
-            }
-        })
-
-
-        const columns = [
-            {accessor: 'id', label:'', priorityLevel: 1, position: 1, minWidth: 150, isVisible: true, sortable: false },
-            { accessor: 'name', label: 'NAME', priorityLevel: 2, position: 2, minWidth: 150, isVisible: true, sortable: false },
-            { accessor: 'busAssigned', label: 'BUS ASSIGNED', priorityLevel: 3, position: 3, minWidth: 150, isVisible: true, sortable: false },
-            { accessor: 'passengers', label: 'PASSENGERS', priorityLevel: 4, position: 4, minWidth: 150, sortable: false },
-            { accessor: 'tripDate', label: 'TRIP DATE', priorityLevel: 5, position: 5, minWidth: 150, isVisible: true, sortable: false },
-            { accessor: 'mileage', label: 'MILEAGE', priorityLevel: 6, position: 6, minWidth: 150, isVisible: true, sortable: false },
-        ]
-
+        const { stations } = this.state 
 
         return (
             <div>
@@ -52,11 +20,6 @@ class Stations extends Component {
                         stations={stations}
                     />
                     <section className='section-container'>
-                    <ReactCollapsingTable 
-                    rows={ rows } 
-                    columns={ columns }
-                    theme= 'my-collapsible-theme'
-                    />
                     </section>
 
                 </div>
